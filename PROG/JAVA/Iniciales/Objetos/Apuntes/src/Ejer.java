3177b40546f67aca3b56fcb5d210a5c3d03647b8
J-osu <josuedelacr96@gmail.com>
2024-10-01 10:59:31 +0200

    Se han hecho los ejercicios 1.1, 1.2 y 1.3 de los apuntes.

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer.java
@@ -1,7 +1,13 @@
 public class Ejer {
     public static void main(String[] args) throws Exception {
         //Punto 1:
-        
+        //1.1
+        //System.out.println("Hola me llamo JOsue y voy a ser tu Dios :)");
+        //1.2
+        // System.out.println("Hola me llamo JOsue y voy a ser tu Dios :)");
+        // System.out.println("Vivo en mi casa, mi numero es mio y ya esta");
+        //1.3
+        System.out.print("Ingles    Castellano");
         //Punto 2:
         //Punto 3:
         //Punto 4:
