1e2bd438e110909aaaea4b0a6ab1c3bab46d06be
J-osu <josuedelacr96@gmail.com>
2024-10-01 13:39:17 +0200

    Se ha añadido el ejercicio 1.4

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer.java
@@ -6,8 +6,20 @@ public class Ejer {
         //1.2
         // System.out.println("Hola me llamo JOsue y voy a ser tu Dios :)");
         // System.out.println("Vivo en mi casa, mi numero es mio y ya esta");
+        //
         //1.3
-        System.out.print("Ingles    Castellano");
+        // System.out.println("┌──────────────────────┐");
+        // System.out.println("│Ingles      Castellano│");
+        // System.out.println("│──────────────────────│");
+        // System.out.printf("│%-10s %11s│\n","Computer","Ordenador");
+        // System.out.printf("│%-10s %11s│\n","Cat","Gato");
+        // System.out.printf("│%-10s %11s│\n","Setudent","Estudiante");
+        // System.out.println("└──────────────────────┘");
+        //
+        //1.4
+        System.out.println("┌─────┬─────┬──────┬─────────┬──────┬───────┐");
+        System.out.println("│Horas│Lunes│Martes│Miercoles│Jueves│Viernes│");
+        System.out.println("│08:00│Digi │");
         //Punto 2:
         //Punto 3:
         //Punto 4:
