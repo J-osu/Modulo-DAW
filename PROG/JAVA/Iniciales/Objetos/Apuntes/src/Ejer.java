99db757e6e4efc1e5a0f0b3029ee3b38fb69cca2
J-osu <josuedelacr96@gmail.com>
2024-10-01 18:11:07 +0200

    Del punto 1 se hicieron los 1.3 y 1.4 y se empezo el punto 2

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer.java
@@ -17,10 +17,40 @@ public class Ejer {
         // System.out.println("└──────────────────────┘");
         //
         //1.4
-        System.out.println("┌─────┬─────┬──────┬─────────┬──────┬───────┐");
-        System.out.println("│Horas│Lunes│Martes│Miercoles│Jueves│Viernes│");
-        System.out.println("│08:00│Digi │");
+        // System.out.println("┌─────┬─────┬──────┬─────────┬──────┬───────┐");
+        // System.out.println("│Horas│Lunes│Martes│Miercoles│Jueves│Viernes│");
+        // System.out.println("├─────┼─────┼──────┼─────────┼──────┼───────┤");
+        // System.out.println("│08:00│Digi │LMSGI │PROG     │ENT   │LMSGI  │");
+        // System.out.println("├─────┼─────┼──────┼─────────┼──────┼───────┤");
+        // System.out.println("│09:00│PROG │LMSGI │PROG     │ENT   │PROG   │");
+        // System.out.println("├─────┼─────┼──────┼─────────┼──────┼───────┤");
+        // System.out.println("│10:00│PROG │SIS   │PROG     │SIS   │PROG   │");
+        // System.out.println("├─────┼─────┼──────┼─────────┼──────┼───────┤");
+        // System.out.println("│10:00│IPPE │SIS   │SIS      │SIS   │PROG   │");
+        // System.out.println("├─────┼─────┼──────┼─────────┼──────┼───────┤");
+        // System.out.println("│10:00│BD   │IPPE  │SOSTE    │BD    │BD     │");
+        // System.out.println("├─────┼─────┼──────┼─────────┼──────┼───────┤");
+        // System.out.println("│10:00│BD   │ENT   │IPPE     │BD    │BD     │");
+        // System.out.println("└─────┴─────┴──────┴─────────┴──────┴───────┘");
+        //
+        //1.7 LUEGO CONTINUO
+
         //Punto 2:
+        //Ejercicio 2.1
+        // int x = 144;
+        // int y = 999;
+        // System.out.println("La x tiene un valor de: "+x+" y la y tiene un valor de: "+y);
+        // int suma = x+y;
+        // int resta = x-y;
+        // int multi = x*y;
+        // double div = (double) x/y;
+        // System.out.println("La suma de los 2 es de: "+suma);
+        // System.out.println("La resta de los 2 es de: "+resta);
+        // System.out.println("La multiplicacion de los 2 es de: "+multi);
+        // System.out.println("La division de los 2 es de: "+div);
+        //
+        //2.2
+        
         //Punto 3:
         //Punto 4:
     }
