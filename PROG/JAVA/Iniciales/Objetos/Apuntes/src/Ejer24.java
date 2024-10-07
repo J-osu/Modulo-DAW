c3c44d41262c7166ae51979f35e99b28c26a419a
J-osu <josuedelacr96@gmail.com>
2024-10-07 17:02:05 +0200

    Se han corregidos los fallos.

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer24.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/Ejer24.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Ejer24 {
     static Scanner sc = new Scanner(System.in);
     static String nombre;
-    static double cargo;
-    static double estado;
+    static int cargo;
+    static int estado;
     static double dias;
     static double sueldo;
     static double sueldodietas;
@@ -12,28 +12,24 @@ public class Ejer24 {
     static double retencion;
     public static void main(String[] args) throws Exception { 
         Ejer24.Datos();
-        Ejer24.sueldo();
         Ejer24.sueldobase();
-        System.out.print("Nombre: "+nombre);
-        System.out.print("Sueldo base: "+sueldo);
-        System.out.print("Dietas: "+dias+" "+sueldodietas);
-        System.out.print("Sueldo bruto: "+sueldobruto);
-        System.out.print("IRPF: "+retencion);
-        System.out.print("Netos: "+neto);
+        Ejer24.CalcularDietas();
+        Ejer24.CalcularSueldoBruto();
+        Ejer24.Calcularretencion();
+        Ejer24.mostrarResultados();
     }
     public static void Datos(){
         System.out.print("introduce tu nombre: ");
         Ejer24.nombre = Ejer24.sc.nextLine();
         System.out.print("Introduce tu cargo en la empresa(1-junior, 2-senior o 3-jefe de proyecto): ");
-        Ejer24.cargo = Ejer24.sc.nextDouble();
+        Ejer24.cargo = Ejer24.sc.nextInt();
         System.out.print("Itroduce los dias que hayas estado fuera: ");
         Ejer24.dias = Ejer24.sc.nextDouble();
         System.out.print("Introduce tu estado civil(1-soltero o 2-casado): ");
-        Ejer24.estado = Ejer24.sc.nextDouble();
+        Ejer24.estado = Ejer24.sc.nextInt();
     }
      public static void sueldobase(){
-        Ejer24.Datos();
-        double sueldo = 0;
+        sueldo = 0;
         if (cargo==1) {
             sueldo = 950;
         } else if (cargo==2) {
@@ -42,20 +38,30 @@ public class Ejer24 {
             sueldo = 1600;
         }
      }
-     public static void retencion(){
-        Ejer24.Datos();
+     public static void CalcularDietas(){
+       sueldodietas = dias * 30;
+     }
+     public static void CalcularSueldoBruto(){
+        sueldobruto = sueldo+sueldodietas;
+     }
+     public static void Calcularretencion(){
         double irpf = 0;
         if (estado==1) {
             irpf = 0.25;
         } else if (estado==2) {
             irpf= 0.20;
         }
-        double retencion = sueldobruto*irpf;
+        retencion = sueldobruto*irpf;
+        neto = sueldobruto-retencion;
      }
-     public static void sueldo(){
-        double sueldobruto = sueldo+sueldodietas;
-        double sueldodietas = dias * 30;
-        double neto = sueldobruto-retencion;
-     }
-     
+     public static void mostrarResultados() {
+        System.out.println("┌─────────────────────────────┐");
+        System.out.println("│Nombre: "+nombre+"                 │");
+        System.out.println("│Sueldo base: "+sueldo +" euros    │");
+        System.out.println("│Dietas: "+dias+" días "+sueldodietas+" euros│");
+        System.out.println("│Sueldo bruto: "+sueldobruto+" euros   │");
+        System.out.println("│Retención IRPF:  "+retencion+" euros │");
+        System.out.println("│Sueldo neto: " + neto + " euros    │");
+        System.out.print("└─────────────────────────────┘");
+    }
 }
