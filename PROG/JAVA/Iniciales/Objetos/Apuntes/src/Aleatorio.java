4a330e7b3ce5e1dc22146962ab0eb3c42306147e
J-osu <josuedelacr96@gmail.com>
2024-10-25 21:23:14 +0200

    -Add Aleatorio.

---
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/Aleatorio.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Aleatorio{
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        Mat.MaxMinTemperaturas();
+    }
+    public static void Clear(){
+    //     System.out.println("");
+    //     System.out.print("Desea continuar s/n: ");;
+    //     char n = Mat.sc.next().toLowerCase().charAt(0);
+    //     if (n == 's'){
+    //         System.out.print("\033[H\033[2J");
+    //         System.out.flush();
+    //   }
+    }
+
+    public static void Ejer2(){
+        //hacer un programa que nos devuelva
+        //el total, recuento de J,K y Q y el maxymin que salga.
+    }
+
+    public static void Ejer5(){
+        System.out.println("algo");
+    }
+
+    public static void Ejer6(){
+        System.out.println("algo");
+    }
+
+    public static void Ejer7(){
+        System.out.println("Algo");
+    }
+    
+    public static void Ejer8(){
+        System.out.println("no");
+    }
+}
+
