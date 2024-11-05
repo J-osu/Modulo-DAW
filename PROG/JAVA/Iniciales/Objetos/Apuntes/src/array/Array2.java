230ae3e7ee243301697bad179a4c72c4851d459f
J-osu <josuedelacr96@gmail.com>
2024-11-05 18:54:30 +0100

    Se ha corregido el codigo.

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/array/Array2.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/array/Array2.java
@@ -4,9 +4,16 @@ public class Array2 {
     static Scanner sc = new Scanner(System.in);
     static int CIUDADES = 5;
     static String ciudades[] = new String[CIUDADES];
-    static double[] temperaturas = new double[21];
+    static double tempperaturas[] = new double[CIUDADES];
+    static double max;
+    static double min;
+    static int Pmax;
+    static int Pmin;
+    static double media;
     public static void main(String[] args) throws Exception {
         Array2.pedirCiudades();
+        Array2.generarTemp();
+        Array2.imprimirResultados();
     }
 
     public static void pedirCiudades(){
@@ -14,37 +21,32 @@ public class Array2 {
             System.out.print("Nombre de la ciudad " + (i + 1) + ": ");
             Array2.ciudades[i] = Array2.sc.next();
         }
-        System.out.println(CIUDADES);
     }
-
-    public static void calcularTempMaxyMin(){
-        double temp[] = new double[CIUDADES];
-        double max = 0; double min = 0; int Pmax = 0; int Pmin = 0; double media = 0;
-        for (int i = 0; i < temperaturas.length; i++) {
-            temperaturas[i] = 20 + i;
-        }
+    public static void generarTemp(){
         for (int i = 0; i<Array2.CIUDADES; i++){
-            
+            double temp = (Math.random()*20)+20;
+            temp = Math.round(temp * 10.0) / 10.0;
+            tempperaturas[i] = temp;
         }
-        max = temp[0];
-        min = temp[0];
-        Pmax = 0;
-        Pmin = 0;
-        for (int i=0; i<Array1.ALUMNOS; i++){
-            media = media + temp[i];
-            if (temp[i]>max){
-                max = temp[i];
+         max = 0; min = 0; Pmax = 0; Pmin = 0; media = 0;
+        max = tempperaturas[0]; min = tempperaturas[0];
+        Pmax = 0; Pmin = 0;
+        for (int i=0; i<Array2.CIUDADES; i++){
+            media = media + tempperaturas[i];
+            if (tempperaturas[i]>max){
+                max = tempperaturas[i];
                 Pmax = i;
             }
-            if (temp[i]<min){
-                min = temp[i];
+            if (tempperaturas[i]<min){
+                min = tempperaturas[i];
                 Pmin = i;
             }
         }
+        media = media / Array2.CIUDADES;
+    }
+    public static void imprimirResultados(){
         System.out.println("Temperatura max :  " + max + " y es de " + Array2.ciudades[Pmax]);
         System.out.println("Temperatura min :  " + min + " y es de " + Array2.ciudades[Pmin]);
-        media = media / Array2.CIUDADES;
         System.out.println("Media : " + media);
-
     }
 }
