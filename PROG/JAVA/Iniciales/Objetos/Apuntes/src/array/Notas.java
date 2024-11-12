925b8d19c45f968375c377643e418ec26b819a9b
J-osu <josuedelacr96@gmail.com>
2024-11-12 13:25:53 +0100

    Se estan realizadno cambios en el fichero Notas.java

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/array/Notas.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/array/Notas.java
@@ -4,23 +4,57 @@ public class Notas {
     static Scanner sc = new Scanner(System.in);
     static int ALUMNOS = 5;
     static int MODULOS = 4;
-    static String[] alumnos = {"José", "Ana", "María", "Luis", "Noelia"};
-    static String[] modulos = {"Programación", "Redes", "Base de datos", "Desarrollo servidor"};
+    static String[] alumnos = { "Jose", "Ana", "Maria", "Luis", "Noelia" };
+    static String[] modulos = { "Programacion", "Redes", "Bases de Datos",  "Desarrollo Servidor" };
     static int[][] notas = new int[ALUMNOS][MODULOS];
-    public static void main(String[] args) throws Exception {
-        //Llama a la función correpomdiemte.
-        Notas.lecturaNotas();
+    
+
+    public static void main(String[] args){
+        Notas.GenerarNotas();
+        Notas.ImprimirNotasTabla();
     }
 
-   public static void lecturaNotas(){
-    for(int i =0; i<ALUMNOS; i++){
-        System.out.println("Nota de " + alumnos[i] + " : " );
-        for(int j=0; i<MODULOS; j++){
-            System.out.println("    -" + modulos[j] + " : ");
+    public static void LecturaNotas(){
+        //recorrido de alumnos
+        for (int i=0; i<ALUMNOS; i++){
+            System.out.println("Nota de " + alumnos[i] + " : "); //1
+            for (int j=0; j<MODULOS; j++){
+                System.out.print("   - " + modulos[j] + " : "); //1
+                // System.out.println("Nota de "+ alumnos[i] + " - " + modulos[j] + " : "); //2
+                //lectura por teclado
+                Notas.notas[i][j]=Notas.sc.nextInt();
+            }
         }
     }
-   }
-   public static void ImprimirNotas(){
+    public static void GenerarNotas(){
+        //recorrido de alumnos
+        for (int i=0; i<ALUMNOS; i++){
+            for (int j=0; j<MODULOS; j++){
+                Notas.notas[i][j]=(int)Math.random()*10;
+            }
+        }
+    }
 
-   }
+    public static void ImprimirNotasTabla(){
+        System.out.println("------ LISTADO DE NOTAS --------------");
+        for (int i=0; i<ALUMNOS; i++){
+            System.out.print("             " + alumnos[i] + "  ");
+        }
+        for (int i=0; i<ALUMNOS; i++){
+            for (int j=0; j<MODULOS; j++){
+                System.out.print("               " + notas[i][j]+ "        ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ImprimirNotas(){
+        System.out.println("------ LISTADO DE NOTAS --------------");
+        for (int i=0; i<ALUMNOS; i++){
+            for (int j=0; j<MODULOS; j++){
+                System.out.println("Nota de "+ alumnos[i] + " - " + modulos[j] + " : " + notas[i][j]) ; //2
+            }
+        }
+    }
+
 }
