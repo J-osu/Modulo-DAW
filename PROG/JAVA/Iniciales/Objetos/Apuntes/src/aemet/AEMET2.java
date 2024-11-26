d23aaf243e84317191973c7e6f1000edadab5969
J-osu <josuedelacr96@gmail.com>
2024-11-26 11:20:00 +0100

    Falta arreglar un fallo.

--- PROG/JAVA/Iniciales/Objetos/Apuntes/src/aemet/AEMET2.java
+++ PROG/JAVA/Iniciales/Objetos/Apuntes/src/aemet/AEMET2.java
@@ -1,20 +1,32 @@
 package aemet;
 import java.util.Scanner;
+import aemet.RegCiudad2;
 
 public class AEMET2 {
     static Scanner sc = new Scanner(System.in);
     static final int NCIUDADES = 8;
-    static String[] ciudades = {"Almeria", "Sevilla", "Granada", "Cordoba", "Cadiz", "Huelva", "Jaen", "Malaga"};
+    public static String[] ciudades = {"Almeria", "Sevilla", "Granada", "Cordoba", "Cadiz", "Huelva", "Jaen", "Malaga"};
     public static RegCiudad2[] regTemperaturas = new RegCiudad2[NCIUDADES];
 
     public static void main(String[] args) {
-        AEMET2.generarRegistroTemp();
+        // AEMET2.GenerarRegitroTemp();
     }
 
-    public static void generarRegistroTemp() {
-        int vmax; int vmin;
-        for(int i =0; i < NCIUDADES; i++){
-            System.out.println(ciudades[i]);
+    // public static void GenerarRegitroTemp(){
+    //     int vmax; int vmin;
+    //     for (int i = 0; i<NCIUDADES; i++){
+    //         System.out.println(ciudades[i] + ": ");
+    //         System.out.print("  Vmax: ");
+    //         vmax = AEMET2.sc.nextInt();
+    //         System.out.print("  Vmin: ");
+    //         vmin = AEMET2.sc.nextInt();
+    //         AEMET2.regTemperaturas[i] = new RegCiudad2(ciudades[i], vmin, vmax);
+    //     }
+    // }
+
+    public static void ListarRegistroTemperaturas(){
+        for (int i=0; i<NCIUDADES; i++){
+            AEMET2.regTemperaturas[i].mostrarTabla();
         }
     }
 }
