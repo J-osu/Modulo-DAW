390bea457364c6e0d5fcf372d4ec7ba240c0cce2
J-osu <josuedelacr96@gmail.com>
2025-02-21 22:05:01 +0100

    +sql

--- BD/actividades-ud5/Alquiler.sql
+++ BD/actividades-ud5/Alquiler.sql
@@ -9,10 +9,10 @@ create table Agencia (
     telefono varchar(15)
 );
 
-insert into agencia values (1, 'Agencia 1', 'Calle 1', '123456789');
-insert into agencia values (2, 'Agencia 2', 'Calle 2', '987654321');
-insert into agencia values (3, 'Agencia 3', 'Calle 3', '456789123');
-insert into agencia values (4, 'Agencia 4', 'Calle 4', '321654987');
+insert into agencia values (1, 'Agencia 1', 'calle-direcion1', '123456789');
+insert into agencia values (2, 'Agencia 2', 'Calle-direcion2', '987654321');
+insert into agencia values (3, 'Agencia 3', 'Calle-direcion3', '456789123');
+insert into agencia values (4, 'Agencia 4', 'Calle-direcion4', '321654987');
 
 create table Propietario(
     id int primary key,
@@ -23,10 +23,10 @@ create table Propietario(
     telefono varchar(15)
 );
 
-insert into Propietario values (1, 'Propietario 1', 'Apellido 1', 'Apellido 2', 'Calle 1', '123456789');
-insert into Propietario values (2, 'Propietario 2', 'Apellido 1', 'Apellido 2', 'Calle 2', '987654321');
-insert into Propietario values (3, 'Propietario 3', 'Apellido 1', 'Apellido 2', 'Calle 3', '456789123');
-insert into Propietario values (4, 'Propietario 4', 'Apellido 1', 'Apellido 2', 'Calle 4', '321654987');
+insert into Propietario values (1, 'Juan', 'Apellido 1', 'Apellido 2', 'Calle-direcion1', '123456789');
+insert into Propietario values (2, 'Ana', 'Apellido 1', 'Apellido 2', 'Calle-direcion2', '987654321');
+insert into Propietario values (3, 'Pepe', 'Apellido 1', 'Apellido 2', 'Calle-direcion3', '456789123');
+insert into Propietario values (4, 'Mario', 'Apellido 1', 'Apellido 2', 'Calle-direcion4', '321654987');
 
 create table Vivienda(
     id int primary key,
@@ -41,10 +41,10 @@ create table Vivienda(
     foreign key (id_agencia) references agencia(id)
 );
 
-insert into Vivienda values (1, 'Vivienda 1', 1, 12345, 'Poblacion 1', 'Piso 1', 1, 1);
-insert into Vivienda values (2, 'Vivienda 2', 2, 54321, 'Poblacion 2', 'Piso 2', 2, 1);
-insert into Vivienda values (3, 'Vivienda 3', 3, 98765, 'Poblacion 3', 'Piso 3', 1, 3);
-insert into Vivienda values (4, 'Vivienda 4', 4, 67890, 'Poblacion 4', 'Piso 4', 2, 4);
+insert into Vivienda values (1, 'Vivienda 1', 1, 12345, 'Almeria', 'Piso 1', 1, 1);
+insert into Vivienda values (2, 'Vivienda 2', 2, 54321, 'Jaen', 'Piso 2', 2, 1);
+insert into Vivienda values (3, 'Vivienda 3', 3, 98765, 'Huelva', 'Piso 3', 1, 3);
+insert into Vivienda values (4, 'Vivienda 4', 4, 67890, 'Almeria', 'Piso 4', 2, 4);
 
 create table Inquilino(
     id int primary key,
@@ -56,10 +56,10 @@ create table Inquilino(
     fecha_nacimiento date
 );
 
-insert into Inquilino values (1, 'Inquilino 1', 'Apellido 1', 'Apellido 2', 'Calle 1', '123456789', '1990-01-01');
-insert into Inquilino values (2, 'Inquilino 2', 'Apellido 1', 'Apellido 2', 'Calle 2', '987654321', '1991-02-02');
-insert into Inquilino values (3, 'Inquilino 3', 'Apellido 1', 'Apellido 2', 'Calle 3', '456789123', '1992-03-03');
-insert into Inquilino values (4, 'Inquilino 4', 'Apellido 1', 'Apellido 2', 'Calle 4', '321654987', '1993-04-04');
+insert into Inquilino values (1, 'Isa', 'Apellido 1', 'Apellido 2', 'Calle 1', '123456789', '1990-01-01');
+insert into Inquilino values (2, 'Pablo', 'Apellido 1', 'Apellido 2', 'Calle 2', '987654321', '1991-02-02');
+insert into Inquilino values (3, 'Tomas', 'Apellido 1', 'Apellido 2', 'Calle 3', '456789123', '1992-03-03');
+insert into Inquilino values (4, 'Isma', 'Apellido 1', 'Apellido 2', 'Calle 4', '321654987', '1993-04-04');
 
 create table Alquiler (
     id int primary key,
@@ -86,12 +86,30 @@ create table Renovada(
 );
 
 insert into Renovada values (1, 1);
-insert into Renovada values (2, 2);
+insert into Renovada values (2, 1);
 insert into Renovada values (3, 3);
-insert into Renovada values (4, 4);
+insert into Renovada values (4, 2);
 
 
 -- Obtener los alquileres que han sido renovados.
 select * from renovada;
 
 --¿Qué propietario tiene más viviendas?
+-- SELECT nombre 
+-- FROM Propietario 
+-- WHERE id = (
+--     SELECT id_propietario 
+--     FROM Vivienda 
+--     GROUP BY id_propietario 
+--     ORDER BY COUNT(*) desc
+--     limit 1
+-- );
+
+--¿Qué agencia oferta viviendas de Almería?
+select id from vivienda where poblacion = 'Almeria';
+
+--Sube el importe del alquiler un 2’5 %
+update alquiler set importe = importe * 1.025;
+
+--Borra las agencias que no ofertan viviendas
+delete from agencia where id not in (select id_agencia from vivienda);
